9d46d71193734915031ec3354cba258aacd8a42d
Sunim Acharya <sunim.54@gmail.com>
2017-11-04 17:03:39 +0545

    Update index

--- functions/index.js
+++ functions/index.js
@@ -1,43 +1,36 @@
-const functions = require('firebase-functions');
-
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 admin.initializeApp(functions.config().firebase);
 
+exports.sendNotification = functions.database
+  .ref("/chat/{uid}/{pushId}")
+  .onWrite(event => {
+    const message = event.data.current.val();
+    const senderUid = message.uid;
+    const receiverUid = event.params.uid;
 
-/**
- * Triggers when new subject added in the list and sends a notification.
- */
-exports.sendNotification = functions.database.ref('/subjects/{subjectKey}').onWrite((event) => {
-  // Grab the current value of what was written to the Realtime Database.
-  const subject = event.data.val();
+    const payload = {
+      notification: {
+        title: senderUid,
+        body: message.text,
+        sound: "default"
+      }
+    };
 
-  // Notification details.
-  const payload = {
-    notification: {
-      title: 'hello',
-      body: `${subject.author} just posted new subject "${subject.title}".`,
-      icon: 'photoURL',
-      sound: 'default',
-      clickAction: 'fcm.ACTION.HELLO',
-      // badge: '1'
-    },
-    data: {
-      extra: 'extra_data',
-    },
-  };
+    const options = {
+        collapseKey: "demo",
+        contentAvailable: true,
+        priority: "high",
+        timeToLive: 60 * 60 * 24
+    };
 
-  // Set the message as high priority and have it expire after 24 hours.
-  const options = {
-    collapseKey: 'demo',
-    contentAvailable: true,
-    priority: 'high',
-    timeToLive: 60 * 60 * 24,
-  };
-
-  // Send a message to devices subscribed to the provided topic.
-  const topic = `/topics/list`;
-  return admin.messaging().sendToTopic(topic, payload, options)
-    .then((response) => {
-      console.log('Successfully sent message:', response);
-    });
-});
+    return admin
+      .messaging()
+      .sendToDevice(receiverUid, payload)
+      .then(function(response) {
+        console.log("Successfully sent message:", response);
+      })
+      .catch(function(error) {
+        console.log("Error sending message:", error);
+      });
+  });
